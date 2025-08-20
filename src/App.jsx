dd1ca8cc005c4a26af5a8621782c614f960c7b12
Fernando Herrero <herrerofernando.94@gmail.com>
2025-08-20 19:44:22 +0200

    feature: add useWindowWidth hook and component as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
 import "./App.css";
 import { Counter } from "./components/Counter.jsx/Counter";
 import { LoginForm } from "./components/LoginForm/LoginForm";
+import { useWindowWidth } from "./hooks/useWindowWidth";
 
 export const App = () => {
+	const width = useWindowWidth();
+
+	useEffect(() => {
+		document.body.style.backgroundColor = width < 768 ? "black" : "orange";
+		document.body.style.color = width < 768 ? "white" : "black";
+	}, [width]);
+
 	return (
 		<>
 			<LoginForm />
